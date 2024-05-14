58642c6481d83c669e9d58430c76aeb61d7e177b
DeVante Farmer <Shine0DYE@gmail.com>
2024-05-14 14:38:41 -0400

    DessertOptions.js created

---
+++ scripts/DessertOptions.js
@@ -0,0 +1,17 @@
+export const dessertChoices = async () => {
+    const response = await fetch("http://localhost:8088/desserts")
+    const desserts = await response.json()
+
+    let dessertChoicesHTML = `<select id='dessert'>
+        <option value='0'>Choose your Dessert:</option>`
+
+        const divStringArray = desserts.map(
+            (dessert) => {
+                return `option value='${dessert.id}'>${dessert.name}</option>`
+            }
+        )
+
+        dessertChoicesHTML += divStringArray.join("")
+        dessertChoicesHTML += "</select>"
+
+}
