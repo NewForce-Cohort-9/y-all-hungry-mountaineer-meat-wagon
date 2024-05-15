4f30dcee7b30d5711711b76f3dcb82320a6cbf0e
Katherine Schloe <KatherineSchloe@outlook.com>
2024-05-15 15:04:20 -0400

    took away experimental code

--- scripts/drinks.js
+++ scripts/drinks.js
@@ -14,26 +14,3 @@ export const drinkChoices = async (locationId) => {
 
     return drinkHTML;
 };
-
-// export const drinkChoices = async (locationId) => {
-//     const response = await fetch(`http://localhost:8088/locationDrinks?truckId=${locationId}`)
-//     const locationDrinks = await response.json() //see if drinks are available at chosen location
-//     const allDrinksResponse = await fetch('http://localhost:8088/drinks')
-//     const allDrinks = await allDrinksResponse.json()
-//
-//     let drinkHTML = `<select id="drink">
-//         <option value="0">Drink options</option>`
-
-//     locationDrinks.forEach((locationDrink) => {
-//         const drink = allDrinks.find((d) => d.id === locationDrink.drinkId)
-//         if (drink) {
-//             drinkHTML += `<option value="${drink.id}" data-quantity="${locationDrink.quantity}">
-//                 ${drink.name} - $${drink.price} - ${drink.description} (${locationDrink.quantity} available)
-//             </option>`;
-//         }
-//     });
-
-//     drinkHTML += `</select>`
-
-//     return drinkHTML
-// };
