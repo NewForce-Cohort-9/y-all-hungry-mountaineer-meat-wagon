8e34452d5bf4cfa26ee963a215927efaf9a3a866
Katherine Schloe <KatherineSchloe@outlook.com>
2024-05-15 13:30:47 -0400

    made it so you can hopefully see the quantity of drinks available

--- scripts/drinks.js
+++ scripts/drinks.js
@@ -1,17 +1,22 @@
 export const drinkChoices = async (locationId) => {
-    const response = await fetch(`http://localhost:8088/drinks?location=${locationId}`);
-    const drinks = await response.json();
+    const response = await fetch(`http://localhost:8088/locationDrinks?truckId=${locationId}`)
+    const locationDrinks = await response.json()
+    const allDrinksResponse = await fetch('http://localhost:8088/drinks')
+    const allDrinks = await allDrinksResponse.json()
 
     let drinkHTML = `<select id="drink">
-<option value="0">Drink options</option>`;
+        <option value="0">Drink options</option>`
 
-    const optionStrings = drinks.map((drink) => {
-        return `<option value="${drink.id}">${drink.name}-$${drink.price}-${drink.description}</option>`;
+    locationDrinks.forEach((locationDrink) => {
+        const drink = allDrinks.find((d) => d.id === locationDrink.drinkId)
+        if (drink) {
+            drinkHTML += `<option value="${drink.id}" data-quantity="${locationDrink.quantity}">
+                ${drink.name} - $${drink.price} - ${drink.description} (${locationDrink.quantity} available)
+            </option>`;
+        }
     });
 
-    drinkHTML += optionStrings.join("");
-    drinkHTML += `</select>`;
+    drinkHTML += `</select>`
 
-    return drinkHTML;
+    return drinkHTML
 };
-
