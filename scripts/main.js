83dac57f303f7df5be2c401bd30ea2c8d67b0760
Abby Trombley <abbyt2024@gmail.com>
2024-05-16 09:41:01 -0400

    none

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { dessertChoices } from "./desserts.js"
 import { drinkChoices } from "./drinks.js";
 import { locationOptions } from "./locationsList.js";
 import { saveSubmission } from "./placeOrder.js";
+import { orderList } from "./saveOrders.js";
 
 const container = document.querySelector("#container")
 
@@ -12,6 +13,7 @@ const render = async () => {
     const dessertChoicesHTML = await dessertChoices()
     const drinkHTML = await drinkChoices()
     const buttonHTML = saveSubmission()
+    const orderListHTML = await orderList()
 
 
     const composedHTML = `
@@ -32,12 +34,13 @@ const render = async () => {
             ${drinkHTML}
             
             ${buttonHTML}
+            ${orderListHTML}
         </section>`
 
     container.innerHTML = composedHTML
 }
 
-// document.addEventListener("updateTransientState", render)
+document.addEventListener("newOrderCreated", render)
 
 render()
 
