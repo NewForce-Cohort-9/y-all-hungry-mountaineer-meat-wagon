0292dcc073971ff06f07a9bc3ed67ff234622862
scof00 <scofprofessional@gmail.com>
2024-05-20 09:45:07 -0400

    Workin on changing quantities

--- scripts/placeOrder.js
+++ scripts/placeOrder.js
@@ -1,13 +1,30 @@
-import { saveOrder } from "./transientState.js";
+import { saveOrder, transientState } from "./transientState.js";
+
+const foodLocationResponse = await fetch("http://localhost:8088/locationFoods");
+const locationFoods = await foodLocationResponse.json();
+
+const drinkLocationResponse = await fetch("http://localhost:8088/locationDrinks");
+const locationDrinks = await drinkLocationResponse.json();
+    
+const dessertLocationResponse = await fetch("http://localhost:8088/locationDesserts");
+const locationDesserts = await dessertLocationResponse.json();
+
 
 const handlePlaceOrderClick = (clickEvent) => {
     if (clickEvent.target.id === "saveSubmission") {
+        // for (const location of locationFoods) {
+        //     if(parseInt(transientState.currentOrder[0].id) === location.foodId && ){
+        //       --location.quantity
+        //     }
+            
+        // }
         saveOrder();
     }
 };
 
 export const saveSubmission = () => {
     document.addEventListener("click", handlePlaceOrderClick)
+    
 
     return "<div></div><button onclick=window.open('http://localhost:3000/receipt') id='saveSubmission'>Place Order</button>"
 }
