fe73bd7575ff21f3365c67b3e45f17d01d960868
scof00 <scofprofessional@gmail.com>
2024-05-20 08:39:21 -0400

    Made tax

--- scripts/saveOrders.js
+++ scripts/saveOrders.js
@@ -12,7 +12,11 @@ export const orderList = async () => {
             order.currentOrder.forEach(item => {
                 orderHTML += `<p><img src="${item.img}" alt="${item.name}" style="width:50px;height:50px;"> ${item.name} - $${item.price}</p>`;
                 subtotal += item.price;
+                
             });
+            const tax = subtotal * 0.06
+            subtotal += tax
+            orderHTML += `<p>Tax (6%): ${tax.toFixed(2)}</p>`
             orderHTML += `<p><strong>Total: $${subtotal.toFixed(2)}</strong></p>`;
             orderHTML += `</div><hr>`;
         }
